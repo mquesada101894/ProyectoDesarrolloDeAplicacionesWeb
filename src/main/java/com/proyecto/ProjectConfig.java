97f0b0356c374c7b84b023d3f6badea0c2a096de
baidor <mmartinez80579@ufide.ac.cr>
2024-11-15 12:25:15 -0600

    Ultima Version

---
+++ src/main/java/com/proyecto/ProjectConfig.java
@@ -0,0 +1,31 @@
+
+package com.proyecto;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+
+@Configuration
+
+public class ProjectConfig implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver(){
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
+        return slr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor LocaleChangeInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+}
