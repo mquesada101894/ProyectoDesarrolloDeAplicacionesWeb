39d14b9221c15aa00e9bc3ca82eecebc98828d00
MauricioPrendasRoldanFidelitas <mprendas20766@ufide.ac.cr>
2024-12-05 21:45:19 -0600

    Adicion de la página tienda

--- src/main/java/com/proyecto/controller/TiendaController.java
+++ src/main/java/com/proyecto/controller/TiendaController.java
@@ -1,7 +1,7 @@
 package com.proyecto.controller;
 
 import com.proyecto.models.Producto;
-import com.proyecto.repositories.ProductoRepository;
+import com.proyecto.services.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,11 +13,11 @@ import java.util.List;
 public class TiendaController {
 
     @Autowired
-    private ProductoRepository productoRepository;
+    private ProductoService productoService;
 
     @GetMapping("/tienda")
     public String mostrarTienda(Model model) {
-        List<Producto> productos = productoRepository.findAll();
+        List<Producto> productos = productoService.obtenerTodosLosProductos();
         model.addAttribute("productos", productos);
         return "tienda";
     }
